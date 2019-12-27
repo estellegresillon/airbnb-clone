487c96a59eefc9f2a76f678ca278c9467d182325
Estelle Grésillon <estelle.gresillon@gmail.com>
2019-12-28 00:24:51 +0100

    various fixes

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,48 +1,48 @@
 import restaurants from "../constants/restaurants";
 
-export function setRestaurants() {
+export const setRestaurants = () => {
   return {
     type: "SET_RESTAURANTS",
     payload: restaurants
   };
 };
 
-export function selectRestaurant(restaurant) {
+export const selectRestaurant = restaurant => {
   return {
     type: "SELECT_RESTAURANT",
     payload: restaurant
   };
 };
 
-export function searchRestaurant(restaurant) {
+export const searchRestaurant = restaurant => {
   return {
     type: "SEARCH_RESTAURANT",
     payload: restaurant
   };
 };
 
-export function sortRestaurants(restaurants) {
+export const sortRestaurants = restaurants => {
   return {
     type: "SORT_RESTAURANTS",
     payload: restaurants
   };
 };
 
-export function centerMapWithLocation(location) {
+export const centerMapWithLocation = location => {
   return {
     type: "CENTER_MAP_WITH_LOCATION",
     payload: location
   };
 };
 
-export function toggleListingAwards(boolean) {
+export const toggleListingAwards = boolean => {
   return {
     type: "TOGGLE_LISTING_AWARDS",
     payload: boolean
   };
 };
 
-export function toggleMap(boolean) {
+export const toggleMap = boolean => {
   return {
     type: "TOGGLE_MAP",
     payload: boolean
