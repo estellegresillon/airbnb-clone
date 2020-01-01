37727854f19f8655361c2e808a350eac8def75c1
Estelle Grésillon <estelle.gresillon@gmail.com>
2020-01-01 12:47:33 +0100

    fixed menu items

--- src/constants/menu-items.js
+++ src/constants/menu-items.js
@@ -1,12 +1,12 @@
 export const menuItems = [
   { name: "Les restos", path: "/restaurants"},
   { name: "Les awards", path: "/awards"},
-  { name: "Proposer un resto", path: "/add-restaurant"},
+  // { name: "Proposer un resto", path: "/add-restaurant"},
 ];
 
 export const menuItemsMobile = [
   { name: "Home", path: "/"},
   { name: "Les restos", path: "/restaurants"},
   { name: "Les awards", path: "/awards"},
-  { name: "Proposer un resto", path: "/add-restaurant"},
+  // { name: "Proposer un resto", path: "/add-restaurant"},
 ];
