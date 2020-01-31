c62fcb1e00a67421f7887ee5c0a98b40f682a486
Estelle Grésillon <estelle.gresillon@gmail.com>
2020-01-31 12:39:44 +0100

    Updates

---
+++ precache-manifest.f5b5feeb37c0e103c4daec7abd57ebaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d571692de0acec8b296bfa121e83c9af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8fb6d3dd1c4ccfbd56c",
+    "url": "/static/css/main.192b32a1.chunk.css"
+  },
+  {
+    "revision": "d39350e7e290aad31813",
+    "url": "/static/js/2.9be168ea.chunk.js"
+  },
+  {
+    "revision": "92fffc35f02a4cf45c0c7ae11109d2c1",
+    "url": "/static/js/2.9be168ea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e8fb6d3dd1c4ccfbd56c",
+    "url": "/static/js/main.a0543cb6.chunk.js"
+  },
+  {
+    "revision": "e9dc661fe0c341d73979",
+    "url": "/static/js/runtime-main.a7417da8.js"
+  }
+]);
